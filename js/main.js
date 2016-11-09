943120554d812566d7e6270dd93db7de7682182b
Chernova Daria <chernovagraphic@gmail.com>
2016-11-09 03:28:08 +0300

    addded new files

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ jQuery(document).ready(function($){
 		updateNavigation();
 	});
 
+
+
 	//smooth scroll to the section
 	navigationItems.on('click', function(event){
         event.preventDefault();
@@ -47,3 +49,43 @@ jQuery(document).ready(function($){
         );
 	}
 });
+
+$(function(){
+		$(".element").typed({
+			strings: ["Multi-disciplined designer from Moscow<br>My skills include: HTML, CSS, Bootstrap, Git, User research, crafting personas, wireframing & prototyping<br>Also looks great naked"],
+			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+			stringsElement: null,
+			// typing speed
+			typeSpeed: 0,
+			// time before typing starts
+			startDelay: 0,
+			// backspacing speed
+			backSpeed: 0,
+			// shuffle the strings
+        		shuffle: false,
+			// time before backspacing
+			backDelay: 500,
+			// loop
+			loop: false,
+			// false = infinite
+			loopCount: false,
+			// show cursor
+			showCursor: true,
+			// character for cursor
+			cursorChar: "|",
+			// attribute to type (null == text)
+			attr: null,
+			// either html or text
+			contentType: 'html',
+			// call when done callback function
+			callback: function() {},
+			// starting callback function before each string
+			preStringTyped: function() {},
+			//callback for every typed string
+			onStringTyped: function() {},
+			// callback for reset
+			resetCallback: function() {}
+		});
+	});
+
+
